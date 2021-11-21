508f53958695affb610528de36c83e035c3b1e21
Wheatley <leon555thai@gmail.com>
2021-11-21 20:40:54 +0700

    remove delay beg

--- src/commands/currency/beg.ts
+++ src/commands/currency/beg.ts
@@ -1,10 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, MessageEmbed } from 'discord.js';
-import { promisify } from 'util';
 import { addBalance, getAccount } from '../../helpers/accountManager';
 import { warningEmbed } from '../../helpers/warningHandler';
-
-const sleep = promisify(setTimeout);
 
 const data = new SlashCommandBuilder()
     .setName('beg')
@@ -14,37 +11,32 @@ async function run(interaction: CommandInteraction) {
     const user = interaction.options?.getUser('user') || interaction.user;
 
     // beg for moeny
-    getAccount(user.id)
-        .then(async (account) => {
-            const balance = account.balance;
-            interaction.deferReply();
-            await sleep(3000);
+    getAccount(user.id).then(async (account) => {
+        const balance = account.balance;
 
-            if (balance <= 10) {
-                const rnd_money = Math.floor(Math.random() * (200 * account.level - (account.level * 50) + 1) + (account.level * 50));
-                addBalance(user.id, rnd_money);
+        if (balance <= 10) {
+            const rnd_money = Math.floor(Math.random() * (200 * account.level - (account.level * 50) + 1) + (account.level * 50));
+            addBalance(user.id, rnd_money);
 
-                const begEmbed = new MessageEmbed()
+            interaction.reply({ embeds: [
+                new MessageEmbed()
                     .setTitle('😥 Poor little beggar, here\'s some money! 😥')
                     .setDescription(`You got **${rnd_money}** 💵 from begging!`)
-                    .setColor(0xF1C40F);
-                interaction.editReply({ embeds: [begEmbed] });
+                    .setColor(0xF1C40F),
+            ] });
 
-            } else if (balance > 0) {
-                const begEmbed = new MessageEmbed()
+        } else if (balance > 0) {
+            interaction.reply({ embeds : [
+                new MessageEmbed()
                     .setTitle('🤬 Go away! 🤬')
                     .setDescription('You already have enough money!')
-                    .setColor(0xE74C3C);
-                interaction.editReply({ embeds: [begEmbed] });
-            }
+                    .setColor(0xE74C3C),
+            ] });
+        }
 
-        }).catch(err => {
-            if (interaction.deferred) {
-                interaction.editReply(warningEmbed({ title: 'ERROR', description: err }));
-            } else {
-                interaction.reply(warningEmbed({ title: 'ERROR', description: err }));
-            }
-        });
+    }).catch(err => {
+        interaction.reply(warningEmbed({ title: 'ERROR', description: err }));
+    });
 }
 
 export default {
