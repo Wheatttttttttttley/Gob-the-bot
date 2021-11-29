b0eb62860ed83a1a62bae5f37067b76640756d0d
Wheatley <leon555thai@gmail.com>
2021-11-29 23:08:29 +0700

    fix: roulette's 'interaciton already deferred'

--- src/commands/roulette/roulette.ts
+++ src/commands/roulette/roulette.ts
@@ -58,7 +58,7 @@ const run = async (interaction: CommandInteraction) => {
             interaction.editReply(warningEmbed({ title: 'Insufficient funds', description: 'You do not have enough chips to make this bet' }));
             return;
         } else if (bet < account.balance / 10) {
-            interaction.reply(warningEmbed({ title: 'TOO LOW BET', description: 'You can\'t bet less than 10% of your current balance' }));
+            interaction.editReply(warningEmbed({ title: 'TOO LOW BET', description: 'You can\'t bet less than 10% of your current balance' }));
             return;
         }
         addBalance(interaction.user.id, -bet);
