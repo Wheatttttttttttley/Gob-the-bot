b882a68342a6ae8e4c6a08d9e56b788ec7cfd180
shalluv <leon555thai@gmail.com>
2022-04-20 19:51:28 +0700

    ♻ restructure daily command

--- src/commands/economy/daily.ts
+++ src/commands/economy/daily.ts
@@ -11,20 +11,26 @@ async function run(interaction: CommandInteraction): Promise<void> {
 
   await getAccount(interaction.user.id)
     .then(async (player: PlayerInt) => {
-      // get the current date + GMT offset (+07:00)
-      const then = new Date(player.cooldown.daily.getTime() + 25200000);
-      const now = new Date(Date.now() + 25200000);
+      if (!player.cooldown.daily) {
+        player.cooldown.daily = new Date(0);
+      }
+      const then = player.cooldown.daily.getTime() + 25200000;
+      const now = Date.now() + 25200000;
+
+      // get rid of hours, minutes, seconds, and milliseconds
+      const prev = then - (then % 86400000);
+      const today = now - (now % 86400000);
 
       // if it's the same day, return
-      if (
-        then.getUTCDate() === now.getUTCDate() &&
-        then.getUTCMonth() === now.getUTCMonth() &&
-        then.getUTCFullYear() === now.getUTCFullYear()
-      ) {
+      if (prev === today) {
+        const diff = today + 86400000 - now;
+        const diffHours = Math.floor(diff / (1000 * 60 * 60));
+        const diffMinutes = Math.floor((diff / (1000 * 60)) % 60);
+        const diffSeconds = Math.floor((diff / 1000) % 60);
         interaction.reply(
           warningEmbed({
             title: "ALREADY CLAIMED",
-            description: `You have already claimed your daily rewards today!`,
+            description: `You've already claimed your daily rewards!\n\nYou can claim again in ${diffHours}h ${diffMinutes}m ${diffSeconds}s.`,
           }),
         );
         return;
@@ -33,13 +39,17 @@ async function run(interaction: CommandInteraction): Promise<void> {
       const payout = 10000 + player.level * 2000;
       addBalance(id, payout);
 
-      await PlayerModel.findOneAndUpdate({ _id: id }, { $set: { cooldown: { daily: new Date() } } }, { upsert: true });
+      await PlayerModel.findOneAndUpdate(
+        { _id: id },
+        { $set: { cooldown: { daily: new Date(today) } } },
+        { upsert: true },
+      );
 
       interaction.reply({
         embeds: [
           new MessageEmbed()
-            .setTitle("✅ SUCCESS")
-            .setDescription(`You have claimed your daily rewards!\nYou have received\n**${payout}** 💵`)
+            .setTitle("✅ SUCCESS ✅")
+            .setDescription(`You successfully claimed your daily rewards!\nYou received **${payout}** 💵`)
             .setColor(0x2ecc71),
         ],
       });
