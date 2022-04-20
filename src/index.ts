a426668001c3317c362a21ff37dc0b471590c3d1
shalluv <leon555thai@gmail.com>
2022-04-20 20:23:39 +0700

    ⏲ increase rest time offset

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { initialize } from "./utils/initialize";
 export const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
   shards: "auto",
-  restTimeOffset: 100,
+  restTimeOffset: 200,
 }) as IClient;
 
 (async (): Promise<void> => {
